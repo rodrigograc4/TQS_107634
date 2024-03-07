604b67971f2f6ae9c31c55964939a020924f2300
rodrigograc4 <rodrigomgraca@ua.pt>
2024-03-07 19:55:11 +0000

    tudo menos eerro null pointer em 3.2

---
+++ lab3/lab3_2/src/test/java/tqs/cars/CarsControllerTest.java
@@ -0,0 +1,117 @@
+package tqs.cars;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import tqs.cars.boundary.CarController;
+import tqs.cars.model.Car;
+import tqs.cars.services.CarManagerService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+@WebMvcTest(CarController.class)
+public class CarsControllerTest {
+    
+
+    @Autowired
+    private MockMvc mvc;
+
+
+    @MockBean
+    private CarManagerService carService;
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+
+    @SuppressWarnings("null")
+    @Test
+    public void testCreateCar() throws Exception {
+        Car car = new Car("Porsche", "GT3");
+        
+        when(carService.save(any())).thenReturn(car);
+
+        mvc.perform(post("/api/cars").contentType(MediaType.APPLICATION_JSON).content(JsonUtils.toJson(car)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.maker", is("Porsche")));
+
+        verify(carService, times(1)).save(any());
+    }
+
+
+    @SuppressWarnings("null")
+    @Test
+    public void testgetAllCars() throws Exception {
+        Car car1 = new Car("Porsche", "GT3");
+        Car car2 = new Car("Dodge", "Charger");
+        Car car3 = new Car ("Nissan", "GTR-35");
+
+        List<Car> cars = Arrays.asList(car1, car2, car3);
+
+        when(carService.getAllCars()).thenReturn(cars);
+
+        mvc.perform(get("/api/cars").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(3)))
+        .andExpect(jsonPath("$[0].maker", is("Porsche")))
+        .andExpect(jsonPath("$[1].maker", is("Dodge")))
+        .andExpect(jsonPath("$[2].maker", is("Nissan")));
+
+
+        verify(carService, times(1)).getAllCars();
+       
+    }
+
+    @Test 
+    public void testGetCarById() throws Exception {
+        Car car = new Car("Porsche", "GT3");
+        car.setCarId(1L);
+
+        when(carService.getCarDetails(1L)).thenReturn(Optional.of(car));
+
+        mvc.perform(get("/api/cars/1").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.maker", is("Porsche")));
+
+        verify(carService, times(1)).getCarDetails(1L);
+    }
+
+    @Test
+    public void testGetCarByIdNotFound() throws Exception {
+        when(carService.getCarDetails(1L)).thenReturn(Optional.empty());
+        
+        mvc.perform(get("/api/cars/1").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    
+        verify(carService, times(1)).getCarDetails(1L);
+    }
+    
+
+
+}
---
+++ lab3/lab3_2/src/test/java/tqs/cars/CarsServiceTest.java
@@ -0,0 +1,88 @@
+package tqs.cars;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import tqs.cars.model.Car;
+import tqs.cars.data.CarRepository;
+import tqs.cars.services.CarManagerService;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(MockitoExtension.class)
+public class CarsServiceTest {
+    
+    @Mock (lenient= true)
+    private CarRepository carRepository;
+
+    @InjectMocks
+    private CarManagerService carService;
+
+
+    @BeforeEach
+    public void setUp(){
+
+        Car car1 = new Car("Porsche", "GT3");
+        Car car2 = new Car("Dodge", "Charger");
+        Car car3 = new Car ("Nissan", "GTR-35");
+
+        car1.setCarId(1L);
+        car2.setCarId(2L);
+        car3.setCarId(3L);
+        List<Car> allCars = List.of(car1, car2, car3);
+
+        when(carRepository.findAll()).thenReturn(allCars);
+        when(carRepository.findByCarId(1L)).thenReturn(car1);
+        when(carRepository.findByCarId(2L)).thenReturn(car2);
+        when(carRepository.findByCarId(3L)).thenReturn(car3);
+        when(carRepository.findByCarId(54L)).thenReturn(null);
+        
+    }
+
+
+    @Test
+    public void testCarDetailsValid(){
+
+        Optional<Car> carToBeFound = carService.getCarDetails(1L);
+
+        assertThat(carToBeFound.get().getMaker()).isEqualTo("Porsche");
+
+    }
+    
+    @Test
+    public void testGetCarDetailsWrong() {
+        Optional<Car> carToBeFound = carService.getCarDetails(54L);
+    
+        if (carToBeFound == null) {
+            fail("carToBeFound is null");
+        }
+        
+        // Adiciona esta verificação adicional
+        assertThat(carToBeFound).isEmpty();
+    }
+
+
+    @Test
+    public void testGetAllCars(){
+            
+            List<Car> cars = carService.getAllCars();
+    
+            assertThat(cars).hasSize(3).extracting(Car::getMaker).contains("Porsche", "Dodge", "Nissan");
+
+            verify(carRepository).findAll();
+    }
+
+}
